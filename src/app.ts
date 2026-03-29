f2a5cbdac9f058045ca5c730465118295fc7bb50
HarmanNahal26 <hnahal@academic.rrc.ca>
2026-03-29 12:53:36 -0500

    updated the app.ts with swagger

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ dotenv.config();
 import cors from "cors";
 import { getHelmetConfig } from "./config/helmetConfig";
 import { getCorsOptions } from "./config/corsConfig";
+
+import setupSwagger from "./config/swagger";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
@@ -25,5 +27,8 @@ app.use(express.json());
 // Routes
 app.use("/api/v1", healthRoutes);
 app.use("/api/v1/events", eventRoutes);
+
+// Setup Swagger
+setupSwagger(app);
 
 export default app;
